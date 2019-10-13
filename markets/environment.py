16b4d125abf28b59b06c8d19d9f130eaeb2296b9
Wolfgang Giersche <wgiersche@gmail.com>
2019-10-13 20:13:56 +0200

    creating training data and MarketFromData

---
+++ markets/environment.py
@@ -0,0 +1,111 @@
+import numpy as np
+from markets.dynamic_market import Ask, Bid, Investor, MomentumInvestor
+
+class TradingEnvironment:
+    """
+    This class represents the RL environment of a single investor. It comprises of a
+    portfolio of holdings - which includes a cash position - and the market with its
+    players, in this case a bunch of momentum investors with limited reason...;-)
+    """
+    def __init__(self, config, holdings, market, tx_cost):
+        self.config = config
+        self.investor = self.create_investor(holdings.copy())
+        self.market = market
+        self.other_investors = make_investors(config['num_investors'], holdings.copy())
+        self.tx_cost = tx_cost
+        
+    def create_investor(self, holdings):
+        cash = holdings['cash']
+        del(holdings['cash'])
+        return Investor("WB", cash, holdings)        
+        
+    def let_others_trade(self):
+        for _ in range(self.config['num_rounds_per_day']):
+            for investor in self.other_investors:
+                investor.act(self.market)
+    
+    def total_wealth(self):
+
+        wealth = self.investor.cash
+        for ticker in self.investor.portfolio:
+            size = self.investor.portfolio[ticker]
+            wealth += size * self.market.price_for(ticker)[1]
+        return round(wealth,3)
+    
+    def normalized_holdings(self):
+        values = {}
+        values['cash'] = self.investor.cash
+        holdings = self.investor.portfolio
+        for ticker in holdings:
+            values[ticker] = holdings[ticker] * self.market.price_for(ticker)[1]
+        w = self.total_wealth()
+        return np.array([v/w for _,v in sorted(values.items())])
+    
+    def act(self, new_weights):
+        """
+        This constitutes a trading day. We act, and then the market moves on.
+        """
+        former_wealth = self.total_wealth()
+    
+        self.market.open()
+        
+        orders = self.create_orders(new_weights)
+        for order in orders:
+            self.market.execute(order)
+
+            total_tx_volume = np.sum(
+                [order.amount * order.price for order in orders])
+        
+        fee = total_tx_volume * self.tx_cost
+        self.investor.cash = round(self.investor.cash - fee, 3)
+        
+        self.let_others_trade()
+        self.market.close()
+        
+        return self.state_rep(former_wealth)
+    
+    def state_rep(self, former_wealth ):
+        h = self.market.history
+        returns = [ np.log(h[key][-1][1] / h[key][-2][1])
+                    for key in h]
+        returns = returns / np.sum(np.abs(returns))
+        observations = np.hstack([self.normalized_holdings(), returns])
+        return (observations, np.log(self.total_wealth()/former_wealth))
+    
+    def create_orders (self, new_weights):
+        """
+        Create an order book from the target portfolio weights
+        new_weights: list of normalized weights of the holdings
+        sorted by the holdings' ticker names, cash last
+        """
+        cur_weights = self.normalized_holdings()
+        vols = ((new_weights - cur_weights) * self.total_wealth())[:-1]
+        holdings = self.investor.portfolio
+        tickers = sorted(holdings)
+        prices = np.array([self.market.price_for(t) for t in tickers])
+
+        # identify the correct prices for bid and ask transactions
+        bid_asks = [p[(v<0).astype(int)] for v,p in zip(vols, prices)]
+
+        orders = []
+        for v, ba, t in zip(vols, bid_asks, tickers):
+            amt = np.abs((v/ba).astype(int))
+            B_or_A = Bid if v>0 else Ask
+            if v != 0:
+                orders.append(B_or_A(price=ba, amount=amt, 
+                                  ticker=t, other_party=self.investor))
+        return orders
+    
+    
+def new_market(): 
+    return Market(stocks=[aapl, msft, tsla], bid_ask=0.1)
+
+def make_investors(num_investors, portfolio=None):
+    from copy import deepcopy
+    investors = []
+    for i in range(num_investors):
+        wr, wm, span = 0.3, 0.15, 20
+        portfolio = deepcopy(portfolio) or {'AAPL': 10000, 'TSLA': 10000, 'MSFT': 10000}
+        investor = MomentumInvestor("m-%s" % i, 1e8, portfolio, wr, wm, span)
+        investors.append(investor)
+    return investors
---
+++ markets/stocks_model.py
@@ -0,0 +1,117 @@
+import numpy as np
+from markets.dynamic_market import Market, Stock, Segment, GeoMarket
+from markets.environment import TradingEnvironment
+
+def rnd_sentiments(s_max = 1, s_min = -10, n_quarters = 16):
+    offset = np.random.randint(64)  # reporting day within the quarter
+    s_c = 0
+    q = 64 # a quarter
+    s = {0: (0, 0)}
+    for i in range(n_quarters):
+        dst = np.random.normal(.006, .005) / q
+        day = i * q + int(np.random.normal(5)) + offset
+        s[day] = (s_c, dst)
+        s_c = min(s_max, s_c + np.random.normal(0, .1))
+        s_c = max(s_min, s_c)
+    s[1024] = (0,0)
+    return s
+
+
+# US market: bulls and bears
+us = GeoMarket('US',                   # A name
+               {0: (.1, 0.001),         # Market sentiment over time, starting slightly bullish
+                150: (-.1, -0.001),
+                300: (-.13, 0),         # Not too bad yet                
+                380: (-.1, -0.005),     # A period of hope
+                450: (.15, 0.003),
+                500: (.25, 0),          # And more hope
+                620: ( .1, 0.004),
+                700: (.1, 0.001),
+                830: (.05, 0.007)
+               })   
+
+
+# Segment sentiment: IT is calming down - the dawn of the new AI winter?
+it = Segment('Information Technology', {0: (.0, .001)})
+
+
+def create_stocks (n_stocks):
+    # creates a set of stock tickers    
+    tickers = list(set(
+    ["".join([chr(int(np.random.random()*26+65)) for i in range(4)]) 
+     for i in range(2*n_stocks)]))[:n_stocks]
+    
+    stocks = {}
+    for ticker in tickers:
+        e_cagr = np.random.normal(6e-2, 4e-2)   # expected compound annual growth rate
+        max_effect = 3.0 # Maximum overrating due to sentiment
+        psi0 = 50 + 20 * np.random.random() # initial price
+        sentiments = rnd_sentiments()       # quarterly impacted stock sentiments
+        beta_geo = np.random.normal(.2, .1)       # exposure (beta) to the US market
+        beta_it = np.random.normal(.0, .5)       # exposure (beta) to the IT sector
+        stock = Stock(name=ticker, e_cagr=e_cagr, max_effect=max_effect, psi0 = psi0,
+                        segments = {it: beta_it}, markets = {us: beta_geo},
+                        sentiments = sentiments, noise=.4)
+        stocks[ticker] = stock
+        
+    return stocks
+
+
+def create_chart_data(stocks, config):
+
+    period = config['num_days']
+    
+    market = Market(stocks=list(stocks.values()), bid_ask=0.1)
+
+    holdings = {ticker: 10000 for ticker in stocks}
+    holdings['cash'] = 1e7
+
+    environment = TradingEnvironment(config, holdings, market, tx_cost=2.5e-3)
+
+    for _ in range(period):
+        market.open()
+        daily = environment.let_others_trade()
+        market.close()
+
+    package = {ticker: {
+        "price": np.array(market.history[ticker][:period])[:,1],
+        "epv": np.array([round(stocks[ticker].psi(t),2) for t in range(period)])}
+        for ticker in stocks}
+
+    return package
+
+
+class MarketFromData:
+    """
+    creates a market wrapper for an array or list of shape [N_STOCKS, N_PRICES]
+    """
+    def __init__(self, data, duration, nh, fee):
+        """
+        data: an array or list of shape [n_stocks, n_prices]
+        nh: max. number of prices in history
+        duration: the length of the period that can be served
+        requires len(data) == duration + nh
+        """
+        self.duration = duration
+        self.nh = nh
+        self.data = np.array(data)
+        self.n_securities = np.shape(self.data)[0]
+        self.fee = fee
+        length = np.shape(self.data)[1]
+        if length != duration + nh:
+            raise ValueError("record length not sum of duration and history.")
+        # Need one more for the log returns
+        np.append(self.data, self.data[:,-1:], axis = -1)
+                    
+    def log_return_history(self, nh, t):
+        if t < 0 or t >= self.duration:
+            raise ValueError("t must be between %s and %s" % (0, self.duration - 1))
+        if nh > self.nh or nh <= 0:
+            raise ValueError("t must be between %s and %s" % (1, self.nh))
+        t += self.nh + 1
+        
+        h = self.data[ :, t-nh-1: t]
+        return np.log(h[:, 1:] / h[:, :-1]).T
+        
+    def prices(self, t):
+        return self.data[:, t]
